be57e74a7c835c1c0a86c2ef051663e851cb34bc
ljs <2447888167@qq.com>
2022-02-06 15:39:23 +0800

    commit

--- domain/model/user.go
+++ domain/model/user.go
@@ -2,11 +2,11 @@ package model
 
 type User struct {
 	//主键
-	ID int64 `gorm:"primary_key;not_null;auto_increment"`
+	ID int64 `gorm:"primary_key;not_null;auto_increment" json:"id"`
 	//用户名称
-	UserName string `gorm:"unique_index;not_null"`
+	UserName string `gorm:"unique_index;not_null" json:"user_name"`
 	//添加需要的字段
-	FirstName string
+	FirstName string `json:"first_name"`
 	//...
 	//密码
 	HashPassword string
